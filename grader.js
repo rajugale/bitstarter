bd64532856ee7c19aeecc1523cf93f4eccabb163
Raj Ugale <raj_ugale@yahoo.com>
2013-07-15 06:40:32 +0000

    Update grader.js to take url arg

--- grader.js
+++ grader.js
@@ -26,6 +26,11 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://murmuring-shore-4535.herokuapp.com";
+
+var sys = require('util');
+var rest = require('restler');
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -61,14 +66,39 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var getResp = function(url, checksfile){
+
+  rest.get(url).on('complete', function(response){
+    console.log(response);
+
+    $ = response;
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+
+    var outJson = JSON.stringify(out, null, 4);
+    console.log(outJson);
+  });
+
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+         .option('-f, --url <urltotest>', 'Path to url')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    if(program.file == undefined){
+       getResp(program.url, program.checks);
+    }else{
+        var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
